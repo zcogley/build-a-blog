c0aeee31976b7bd368e2787bc293a19781959123
Zac Cogley <zaccogley@gmail.com>
2017-02-06 12:42:52 -0600

    newpost working (badly)

--- main.py
+++ main.py
@@ -14,12 +14,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class BlogHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render("blog.html")
+
+class PostHandler(Handler):
+    def get(self):
+        self.render("newpost.html")
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Handler),
+    ('/blog', BlogHandler),
+    ('/newpost', PostHandler)
 ], debug=True)
